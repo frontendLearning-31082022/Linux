8d94975fc0e5af3f921da9c27d004ee7ec8d44d0
testoccgg <testoccgg@gmail.com>
2023-05-04 15:44:50 +0300

    refactor INSERT using table names now

--- mysql/fill/fill.sh
+++ mysql/fill/fill.sh
@@ -3,15 +3,11 @@ function normalRegex_PyScript() {
 [ -e regex_matches_array.py ] && return
 echo  "import re
 import sys
-
-regex= sys.argv[1] 
-test_str= sys.argv[2] 
-
+regex= sys.argv[1]
+test_str= sys.argv[2]
 #print(regex)
 #print(test_str)
-
 matches = re.finditer(regex, test_str, re.MULTILINE)
-
 for matchNum, match in enumerate(matches, start=1):
     print(match.group())" > regex_matches_array.py
 }
@@ -26,17 +22,28 @@ table=$(head -n 1 CreateTable)
 create_table="use $db; $(cat CreateTable)"
 
 columns_schema=$(cat CreateTable)
-columns=$(python3 regex_matches_array.py '^\s*[^CREATE][A-z]+\s' "$columns_schema" )
-count_columns="$(echo "$columns" | wc -l)"
-autoincrement_colums=$(python3 regex_matches_array.py 'AUTO_INCREMENT' "$columns_schema" )
-count_auto_increment=${#autoincrement_colums[*]}
-input_data_columns_count=$(($count_columns-$count_auto_increment+1))
+columns_string=$(python3 regex_matches_array.py '^\s*[^CREATE][A-z]+\s.*' "$columns_schema" )
+readarray -t columns_all <<<"$columns_string"
 
+columns=()
+not_insert=("AUTO_INCREMENT" "AS")
+for column_string in "${columns_all[@]}"
+do
+	stopword=false
+	for word in "${not_insert[@]}"
+	do
+		[[ $column_string =~ (.*$word.*) ]] && stopword=true
+	done
+	column_string=$(python3 regex_matches_array.py '^\s*([A-z_]*)' "$column_string" )
+   ! $stopword && columns+=($column_string)
+done
+#printf '%s ' "${columns[@]}"
+
+input_data_columns_count=$((${#columns[*]}+1))
 validated_input_data=true
 line_num=0
 while IFS= read -r line; do
     [[ line_num%input_data_columns_count!=0 ]] && continue
-    echo $line || 
     [[ ${#line}>0 ]] && validated_input_data=false && \
     echo "incorrect input data structure" && exit 1
     line_num=$((line_num++))
@@ -45,24 +52,30 @@ done < data
 mysql -e "CREATE DATABASE IF NOT EXISTS ${db}"
 mysql -u$user -p -e "$create_table"
 
-#TODO no autoincrement - NULL
+first_part_insert="use $db; INSERT INTO $table ( $(printf '%s, ' "${columns[@]}")"
+first_part_insert=${first_part_insert::-2}
+first_part_insert="first_part_insert ) VALUES("
+
 list_inserts=""
-value_to_insert="use $db; INSERT INTO $table VALUES(NULL,"
+value_to_insert=first_part_insert
 line_num=0
 while IFS= read -r line; do
     line_num=$((line_num+1))
-    if [ $(($line_num % $input_data_columns_count)) == 0 ] 
+    if [ $(($line_num % $input_data_columns_count)) == 0 ]
     then
     	value_to_insert=${value_to_insert::-2}
     	value_to_insert="$value_to_insert);"
-    	
     	list_inserts="$list_inserts\n$value_to_insert"
     	echo -e "$value_to_insert"
-
-	value_to_insert="use $db; INSERT INTO $table VALUES(NULL,"
+	    value_to_insert=first_part_insert
     	continue;
     fi
     value_to_insert="$value_to_insert '$line', "
 done < data
+
+#TODO to funcMove
+value_to_insert=${value_to_insert::-2}
+value_to_insert="$value_to_insert);"
+list_inserts="$list_inserts\n$value_to_insert"
 
 mysql -u$user -p -e "$list_inserts"
